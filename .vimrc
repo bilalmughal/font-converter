17fb26916319d48640e3e79ec0830bf17b84bb52
Mirza Bilal <bilal@mirzabilal.com>
2015-01-13 03:45:08 +0500

    ttf to otf converter added with unit test Project vimrc file added

---
+++ .vimrc
@@ -0,0 +1,9 @@
+" Ignore in nerd tree
+let NERDTreeIgnore = ['\.cmake$', 'CMakeCache.txt', 'CMakeFiles', 'tags']
+
+set tabstop=4				" a tab is four spaces
+set shiftwidth=4 		" number of spaces for autoindenting
+
+" Tags
+set tags+=~/.vim/tags/tags-fontforge
+set tags+=~/.vim/tags/tags-cxxtest
--- test/src/ff-test.h
+++ test/src/ff-test.h
@@ -1,3 +1,22 @@
+/* 
+*  Copyright © 2012, Mirza Bilal (bilal@mirzabilal.com) All rights reserved.
+*  Redistribution and use in source and binary forms, with or without modification, are permitted provided that 
+*  the following conditions are met:
+*   1.  Redistributions of source code must retain the above copyright notice, this list of conditions and the
+*       following disclaimer.
+*   2.  Redistributions in binary form must reproduce the above copyright notice, this list of conditions and 
+*      the following disclaimer in the documentation and/or other materials provided with the distribution.
+*   3.  Neither the name of Mirza Bilal nor the names of its contributors may be used to endorse or promote 
+*       products derived from this software without specific prior written permission.
+*  THIS SOFTWARE IS PROVIDED BY MIRZA BILAL "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
+*  TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+*  SHALL MIRZA BILAL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+*  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+*  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
+*  TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+*  POSSIBILITY OF SUCH DAMAGE.
+*/
+
 /*
  * =====================================================================================
  *
@@ -35,19 +54,29 @@ class FFTestSuite : public CxxTest::TestSuite
 
         void test_LoadFont( void )
         {
+            TS_TRACE("Starting Test LoadFont");
             void* res = LoadFont("test/data/yoo.ttf");
             TS_ASSERT(res);
         }
 
         void test_fontConvert_ttf2woff( void )
         {
+            TS_TRACE("Starting Test ttf2woff");
             int res = convert_font("test/data/yoo.ttf", "woff", "bin/result");
             TS_ASSERT_EQUALS( res, 0 );
         }
 
         void test_fontConvert_woff2ttf( void )
         {
+            TS_TRACE("Starting Test woff2ttf");
             int res = convert_font("test/data/yoo.woff", "ttf", "bin/result");
+            TS_ASSERT_EQUALS( res, 0 );
+        }
+        
+        void test_fontConvert_ttf2otf( void )
+        {
+            TS_TRACE("Starting Test ttf2otf");
+            int res = convert_font("test/data/yoo.ttf", "otf", "bin/result");
             TS_ASSERT_EQUALS( res, 0 );
         }
 };
